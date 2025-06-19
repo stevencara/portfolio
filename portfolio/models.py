4194480efdc44747ab3c56567c6794c3f876f4e6
stevencara2022 <stevencara2022@gmail.com>
2025-06-18 19:14:59 -0500

    prueba cloudinary 7

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.db.models.fields import CharField, URLField
 from django.db.models.fields.files import ImageField 
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
 class Project(models.Model):
     title = CharField(max_length=100)
     description = CharField(max_length=250)
-    image = ImageField(upload_to='portfolio/images/')
+    image = CloudinaryField('image')
     url = URLField(blank=True)
