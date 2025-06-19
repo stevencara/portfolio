b97cf0daf5ee2b10caf20c45ddebb5626bed41c0
stevencara2022 <stevencara2022@gmail.com>
2025-06-18 19:07:08 -0500

    prueba cloudinary 6

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 import datetime 
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    image = models.ImageField(upload_to="blog/images")
+    image = CloudinaryField('image')
     date = models.DateField(datetime.date.today)
